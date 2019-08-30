d010feb726f19aded84f8cb5fedb69fb5552de54
Wiebe <43444455+ConnectedWiebe@users.noreply.github.com>
2019-08-30 12:27:54 +0200

    Fix bug in disposable resources autofac registration

--- samples/WebApi.Client.Sample/Autofac/Registrations/DisposableResourcesRegistration.cs
+++ samples/WebApi.Client.Sample/Autofac/Registrations/DisposableResourcesRegistration.cs
@@ -24,7 +24,7 @@ namespace Informapp.InformSystem.WebApi.Client.Sample.Autofac.Registrations
 
             builder.RegisterType<DisposableResources>()
                 .As<IDisposableResources>()
-                .InstancePerRequest();
+                .InstancePerDependency();
         }
     }
 }
