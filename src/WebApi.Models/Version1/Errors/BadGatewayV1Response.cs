86b4855305924d88865ca84bc78c4e74bcfc7ce5
Wiebe <43444455+ConnectedWiebe@users.noreply.github.com>
2019-08-30 14:27:15 +0200

    Add bad gateway response

---
+++ src/WebApi.Models/Version1/Errors/BadGatewayV1Response.cs
@@ -0,0 +1,20 @@
+﻿using Informapp.InformSystem.WebApi.Models.ExampleValues;
+using Informapp.InformSystem.WebApi.Models.Version1.Constants;
+using System.Runtime.Serialization;
+
+namespace Informapp.InformSystem.WebApi.Models.Version1.Errors
+{
+    /// <summary>
+    /// Version 1 bad gateway response
+    /// </summary>
+    [DataContract(Namespace = Version1Constants.Namespace)]
+    public partial class BadGatewayV1Response
+    {
+        /// <summary>
+        /// Message
+        /// </summary>
+        [DataMember]
+        [ExampleValue("Received an invalid response from the upstream server.")]
+        public string Message { get; set; }
+    }
+}
---
+++ src/WebApi.Models/Version2/Errors/BadGatewayV2Response.cs
@@ -0,0 +1,20 @@
+﻿using Informapp.InformSystem.WebApi.Models.ExampleValues;
+using Informapp.InformSystem.WebApi.Models.Version1.Constants;
+using System.Runtime.Serialization;
+
+namespace Informapp.InformSystem.WebApi.Models.Version2.Errors
+{
+    /// <summary>
+    /// Version 2 bad gateway response
+    /// </summary>
+    [DataContract(Namespace = Version1Constants.Namespace)]
+    public partial class BadGatewayV2Response
+    {
+        /// <summary>
+        /// Message
+        /// </summary>
+        [DataMember]
+        [ExampleValue("Received an invalid response from the upstream server.")]
+        public string Message { get; set; }
+    }
+}
