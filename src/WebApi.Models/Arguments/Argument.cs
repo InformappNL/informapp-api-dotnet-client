adc7f329d6edf85a629610e24ff99cc061756568
Wiebe <43444455+ConnectedWiebe@users.noreply.github.com>
2019-08-30 14:33:43 +0200

    Add argument class to models project

---
+++ src/WebApi.Models/Arguments/Argument.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Informapp.InformSystem.WebApi.Models
+{
+    /// <summary>
+    /// Static class with methods to ensure conditions for arguments are true
+    /// </summary>
+    [DebuggerStepThrough]
+    internal static class Argument
+    {
+        /// <summary>
+        /// Ensure an instance is not null
+        /// </summary>
+        /// <typeparam name="T">Type of object to check</typeparam>
+        /// <param name="instance">The instance to check</param>
+        /// <param name="parameterName">The name of the parameter</param>
+        /// <exception cref="ArgumentNullException"><paramref name="instance"/> is null</exception>
+        public static void Required<T>(T instance, string parameterName)
+        {
+            if (IsNull(instance) == true)
+            {
+                ThrowNull(parameterName);
+            }
+        }
+
+        /// <summary>
+        /// Ensure a reference type instance is not null
+        /// </summary>
+        /// <typeparam name="T">Type of object to check</typeparam>
+        /// <param name="instance">The instance to check</param>
+        /// <param name="parameterName">The name of the parameter</param>
+        /// <exception cref="ArgumentNullException"><paramref name="instance"/> is null</exception>
+        public static void NotNull<T>(T instance, string parameterName)
+            where T : class
+        {
+            if (instance is null)
+            {
+                ThrowNull(parameterName);
+            }
+        }
+
+        /// <summary>
+        /// Ensure an nullable value type instance is not null
+        /// </summary>
+        /// <typeparam name="T">Type of object to check</typeparam>
+        /// <param name="instance">The instance to check</param>
+        /// <param name="parameterName">The name of the parameter</param>
+        /// <exception cref="ArgumentNullException"><paramref name="instance"/> is null</exception>
+        public static void NotNull<T>(T? instance, string parameterName)
+            where T : struct
+        {
+            if (instance.HasValue == false)
+            {
+                ThrowNull(parameterName);
+            }
+        }
+
+        /// <summary>
+        /// Ensure a string is not null or empty
+        /// </summary>
+        /// <param name="instance">The instance to check</param>
+        /// <param name="parameterName">The name of the parameter</param>
+        /// <exception cref="ArgumentNullException"><paramref name="instance"/> is null</exception>
+        /// <exception cref="ArgumentException"><paramref name="instance"/> is empty</exception>
+        public static void NotNullOrEmpty(string instance, string parameterName)
+        {
+            if (IsNull(instance) == true)
+            {
+                ThrowNull(parameterName);
+            }
+
+            if (string.IsNullOrEmpty(instance) == true)
+            {
+                ThrowEmpty(parameterName);
+            }
+        }
+
+        /// <summary>
+        /// Ensure a collection is not null or empty
+        /// </summary>
+        /// <typeparam name="T">Type of objects in the collection</typeparam>
+        /// <param name="instance">The instance to check</param>
+        /// <param name="parameterName">The name of the parameter</param>
+        /// <exception cref="ArgumentNullException"><paramref name="instance"/> is null</exception>
+        /// <exception cref="ArgumentException"><paramref name="instance"/> is empty</exception>
+        public static void NotNullOrEmpty<T>(IEnumerable<T> instance, string parameterName)
+        {
+            if (IsNull(instance) == true)
+            {
+                ThrowNull(parameterName);
+            }
+
+            if (instance.Any() == false)
+            {
+                ThrowEmpty(parameterName);
+            }
+        }
+
+        private static bool IsNull<T>(T instance)
+        {
+#pragma warning disable IDE0041 // Null check can be simplified -- avoiding == operator in case type T has an overload.
+            return ReferenceEquals(null, instance);
+#pragma warning restore IDE0041
+        }
+
+        private static void ThrowNull(string parameterName)
+        {
+            throw new ArgumentNullException(parameterName);
+        }
+
+        private static void ThrowEmpty(string parameterName)
+        {
+            throw new ArgumentException("Can not be empty", parameterName);
+        }
+    }
+}
