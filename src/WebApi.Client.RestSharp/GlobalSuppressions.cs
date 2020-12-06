e3df59ffaad289df165648fd4482546963053f9e
Wiebe <43444455+ConnectedWiebe@users.noreply.github.com>
2020-12-06 19:22:17 +0100

    Cleanup GlobalSuppressions files

--- src/WebApi.Client.RestSharp/GlobalSuppressions.cs
+++ src/WebApi.Client.RestSharp/GlobalSuppressions.cs
@@ -1,17 +1,1 @@
-﻿using Informapp;
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("", "CA0000:Description", Justification = "TODO", Scope = "namespaceanddescendants", Target = GlobalSuppressions.ResourceNameSpace)]
-
-namespace Informapp
-{
-    internal static class GlobalSuppressions
-    {
-        public const string ResourceNameSpace =
-            nameof(Informapp) + "." +
-            nameof(InformSystem) + "." +
-            nameof(InformSystem.WebApi) + "." +
-            nameof(InformSystem.WebApi.Client) + "." +
-            nameof(InformSystem.WebApi.Client.RestSharp);
-    }
-}
+﻿
