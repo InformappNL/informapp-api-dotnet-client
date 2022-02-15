35278e8553323c5c43c9646f803d46b1ee10df0b
Wiebe <43444455+ConnectedWiebe@users.noreply.github.com>
2022-02-15 18:10:52 +0100

    Add serialization extension methods to logger

---
+++ src/IntegrationTool.Core/Loggers/ApplicationLoggerExtensions.Serialize.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+
+namespace Informapp.InformSystem.IntegrationTool.Core.Loggers
+{
+    /// <summary>
+    /// Application logger extension methods for serialization
+    /// </summary>
+    public static partial class ApplicationLoggerExtensions
+    {
+        /// <summary>
+        /// Serialize an instance
+        /// </summary>
+        /// <typeparam name="T">The type of object to serialize</typeparam>
+        /// <param name="logger">The logger instance</param>
+        /// <param name="value">The instance to serialize</param>
+        /// <returns>The serialized value</returns>
+        public static string Serialize<T>(this IApplicationLogger logger, T? value)
+            where T : struct
+        {
+            Argument.NotNull(logger, nameof(logger));
+
+            string serialized = string.Empty;
+
+            if (value.HasValue)
+            {
+                serialized = JsonConvert.SerializeObject(value);
+            }
+
+            return serialized;
+        }
+
+        /// <summary>
+        /// Serialize an instance
+        /// </summary>
+        /// <typeparam name="T">The type of object to serialize</typeparam>
+        /// <param name="logger">The logger instance</param>
+        /// <param name="value">The instance to serialize</param>
+        /// <returns>The serialized value</returns>
+        public static string Serialize<T>(this IApplicationLogger logger, T value)
+            where T : class
+        {
+            Argument.NotNull(logger, nameof(logger));
+
+            string serialized = string.Empty;
+
+            if (value != null)
+            {
+                serialized = JsonConvert.SerializeObject(value);
+            }
+
+            return serialized;
+        }
+    }
+}
